90f5743b111beb74d915c93e8d8df0d06d0040b3
Shubham Chhetri <105001222+shubhamchhetri142@users.noreply.github.com>
2022-05-28 17:04:14 +0530

    Update StreamsDemo.java

--- streams/StreamsDemo.java
+++ streams/StreamsDemo.java
@@ -14,20 +14,20 @@ public class StreamsDemo {
 		List<String> listoutput = citynames.stream().filter(names -> names.length() > 6)
 				.map(names -> names.toUpperCase()).collect(Collectors.toList());
 		System.out.println(listoutput);
-		List<String> startsc = citynames.stream().filter(names -> names.startsWith("C"))
+		List<String> startsclist = citynames.stream().filter(names -> names.startsWith("C"))
 				.map(names -> names.toUpperCase()).collect(Collectors.toList());
 		System.out.println(startsc);
-		List<String> contains = citynames.stream().filter(names -> names.contains("ai"))
-				.map(names -> names.toLowerCase()).collect(Collectors.toList());
+		List<String> containslist = citynames.stream()
+				.map(names -> names.concat(" Metro")).collect(Collectors.toList());
 		System.out.println(contains);
-//		List<String> append = citynames.stream().filter(names -> names.concat("Metro"))
-//				.map(names -> names.toUpperCase()).collect(Collectors.toList());
+		List<String> append = citynames.stream().filter(names -> names.concat("Metro"))
+				.map(names -> names.toUpperCase()).collect(Collectors.toList());
 		List<String> sort = citynames.stream().sorted().collect(Collectors.toList());
 		System.out.println(sort);
 		long count = citynames.stream().filter(names -> names.length() > 0).count();
 		System.out.println(count);
-		// List<String>
-		// first=citynames.stream().findFirst().map(names->names.toLowerCase())
+		List<String> firstlist=citynames.stream().findFirst().get();
+		System.out.println(firstlist);
 	}
 
 }
