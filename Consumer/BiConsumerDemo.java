c23a4b313711fc3153b6edee836f99493a511882
Shubham Chhetri <105001222+shubhamchhetri142@users.noreply.github.com>
2022-05-30 15:01:12 +0530

    Add files via upload

--- Consumer/BiConsumerDemo.java
+++ Consumer/BiConsumerDemo.java
@@ -1,14 +1,34 @@
 package com.java8.Consumer;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BiConsumerDemo {
+	static BiConsumer<String, String> listhobbies = (name, hobbies) -> System.out
+			.println("Name:" + name + "     " + "Hobbies:" + hobbies.toUpperCase());
+	static List<Person> listperson = PersonRepository.getAllPersons();
+
+	static void getHobbies() {
+		listperson.forEach((details) -> {
+			details.getHobbies().forEach((hobbies) -> {
+				listhobbies.accept(details.getName(), hobbies);
+			});
+		});
+	}
+
+	static BiConsumer<String, Double> income = (name, salary) -> System.out
+			.println("Name: " + name + "     " + "salary:" + (salary + 1000));
+
+	static void getSalary() {
+		listperson.forEach((details) -> {
+			income.accept(details.getName(), details.getSalary());
+		});
+	}
 
 	public static void main(String[] args) {
-		BiConsumer<Integer, Integer> addition = (a, b) -> System.out.println("Addtion of two Numbers:" + (a + b));
-		addition.accept(5, 10);
-		BiConsumer<String, String> name = (a, b) -> System.out.println("Nmae of the Person:" + (a + b));
-		name.accept("Shubham", " Chhetri");
+		getHobbies();
+		getSalary();
+
 	}
 
 }
